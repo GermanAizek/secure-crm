a0f76cd174b3d5665ff817a394f74c3bf0f078fa
GermanAizek <GermanAizek@yandex.ru>
2019-09-13 00:11:50 +0300

    [ADD] Added config.php

---
+++ config.php
@@ -0,0 +1,11 @@
+<?php
+
+error_reporting(E_ALL);
+
+// connect to db
+$SERVER_DATABASE = "[ДАННЫЕ УДАЛЕНЫ]";
+$USERNAME_DATABASE = "[ДАННЫЕ УДАЛЕНЫ]";
+$PASSWORD_DATABASE = "[ДАННЫЕ УДАЛЕНЫ]";
+$NAME_DATABASE = "[ДАННЫЕ УДАЛЕНЫ]";
+
+?>
