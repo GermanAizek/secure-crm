b9cc4e3197c260a69e4ec7130413f8151980d862
GermanAizek <GermanAizek@yandex.ru>
2019-11-16 03:10:56 +0300

    [ADD] Added client config (store path to theme, include js thirdparty)

---
+++ client_config.php
@@ -0,0 +1,13 @@
+<?php
+
+if (!defined('THEME_PATH')) {
+    define('THEME_PATH', '/content/themes/'); // dirname(__DIR__)
+}
+
+if (!defined('INCLUDE_PATH')) {
+    define('INCLUDE_PATH', '/include/'); // dirname(__DIR__)
+}
+
+$THEME_ACTIVE = 'sb-admin-2';
+
+?>
