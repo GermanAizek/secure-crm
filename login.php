30328e2f5a7a91af02ef2bb7e2d3c269bae9ee84
GermanAizek <GermanAizek@yandex.ru>
2019-11-16 03:15:48 +0300

    [ADD] Added htmlspecialchars post params

--- login.php
+++ login.php
@@ -1,43 +1,49 @@
 <?php
 session_start();
 
-include("config.php");
+if (isset($_SESSION['user_id'])) {
+	header("Location: /main.html");
+} else {
+	include("config.php");
 
-if (isset($_POST["usr"]) and isset($_POST["pwd"])) {
-	// connect to db
-	try {
-	    $conn = new PDO("mysql:host=$SERVER_DATABASE;dbname=$NAME_DATABASE;charset=utf8", $USERNAME_DATABASE, $PASSWORD_DATABASE);
-	    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+	if (isset($_POST["usr"]) and isset($_POST["pwd"])) {
+		$user = htmlspecialchars($_POST["usr"], ENT_QUOTES);
+		$pass = htmlspecialchars($_POST["pwd"], ENT_QUOTES);
+		// connect to db
+		try {
+		    $conn = new PDO("mysql:host=$SERVER_DATABASE;dbname=$NAME_DATABASE;charset=utf8", $USERNAME_DATABASE, $PASSWORD_DATABASE);
+		    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-	    $conn->beginTransaction();
+		    $conn->beginTransaction();
 
-		$stmt = $conn->prepare("SELECT * FROM users WHERE login=? and password=?");
-		$stmt->execute(array(
-			$_POST['usr'],
-			$_POST['pwd']
-		));
-		$result = $stmt->fetch();
-
-	    if ($result == 0) {
-	    	print json_encode('invalid');
-	    } else {
-	    	$_SESSION['user_id'] = $result['id'];
-	    	$_SESSION['user'] = $_POST['usr'];
-
-	    	$stmt = $conn->prepare("UPDATE users SET last_date=now() WHERE login=? and password=?");
+			$stmt = $conn->prepare("SELECT * FROM users WHERE login=? and password=?");
 			$stmt->execute(array(
-				$_POST['usr'],
-				$_POST['pwd']
+				$user,
+				$pass
 			));
+			$result = $stmt->fetch();
 
-	    	print json_encode('suckcess');
-	    }
+		    if ($result == 0) {
+		    	print json_encode('invalid');
+		    } else {
+		    	$_SESSION['user_id'] = $result['id'];
+		    	$_SESSION['user'] = $user;
 
-	    $conn->commit();
-	}
-	catch(PDOException $e) {
-	    print json_encode($e);
-	    $conn->rollBack();
+		    	$stmt = $conn->prepare("UPDATE users SET last_date=now() WHERE login=? and password=?");
+				$stmt->execute(array(
+					$user,
+					$pass
+				));
+
+		    	print json_encode('suckcess');
+		    }
+
+		    $conn->commit();
+		}
+		catch(PDOException $e) {
+			$conn->rollBack();
+		    print json_encode($e);
+		}
 	}
 }
  
